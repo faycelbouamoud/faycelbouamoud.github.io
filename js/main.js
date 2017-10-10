3ece2e0bce50c6f9488108e36c33d4cd9126e127
Faycel BOUAMOUD <faycel.bouamoud@esprit.tn>
2017-10-10 02:32:31 +0200

    some modifs

--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
-function toggleMenu() {
-	if ($(".lateral-menu").is(":visible") == false) {
-		$(".lateral-menu").fadeIn("slow");
-		$(".menu").css({"height":"40em"});
-	} else {
-		$(".lateral-menu").fadeOut("slow");
-		$(".menu").css({"height":"4em"});
+var toggleMenu = function() {
+	console.log($(window).width() );
+	console.log(( $(window).width() >= 320 ) && ( $(window).width() <= 768 ));
+	if( ( $(window).width() >= 320 ) && ( $(window).width() <= 768 ) ) {
+			if ($(".lateral-menu").is(":visible") == false) {
+				$(".lateral-menu").fadeIn(0);
+				$(".menu").css({"height":"40em"});
+			} else {
+				$(".lateral-menu").fadeOut(0);
+				$(".menu").css({"height":"4em"});
+			}
+		}
+}  
+
+$(window).resize(function() {
+	console.log($(window).width() );
+	if(  ( $(window).width() > 768 ) ) {
+		console.log($(window).width() );
+		$(".lateral-menu").css({"display":"block"});
 	}
-}
+})
+
+$(window).resize(function() {
+	console.log($(window).width() );
+	if(  ( $(window).width() < 769 ) ) {
+		console.log($(window).width() );
+		$(".lateral-menu").css({"display":"none"});
+	}
+})
+  
